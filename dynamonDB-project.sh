806631a57321ff72e14cc38cb963179bd6c1cc07
g4bri0s <g.cristianods@gmail.com>
2023-03-10 18:14:15 +0000

    first and last commit

---
+++ dynamonDB-project.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#Criando uma tabela
+
+aws dynamodb create-table \
+    --table-name Music \
+    --attribute-definitions \
+        AttributeName=Artist,AttributeType=S \
+        AttributeName=SongTitle,AttributeType=S \
+    --key-schema \
+        AttributeName=Artist,KeyType=HASH \
+        AttributeName=SongTitle,KeyType=RANGE \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
+
+#Criar um index global secundário baeado no título do álbum
+
+aws dynamodb update-table \
+    --table-name Music \
+    --attribute-definitions AttributeName=AlbumTitle,AttributeType=S \
+    --global-secondary-index-updates \
+        "[{\"Create\":{\"IndexName\": \"AlbumTitle-index\",\"KeySchema\":[{\"AttributeName\":\"AlbumTitle\",\"KeyType\":\"HASH\"}], \
+        \"ProvisionedThroughput\": {\"ReadCapacityUnits\": 10, \"WriteCapacityUnits\": 5      },\"Projection\":{\"ProjectionType\":\"ALL\"}}}]"
+
+#Criar um index global secundário baseado no nome do artista e no título do álbum
+
+aws dynamodb update-table \
+    --table-name Music \
+    --attribute-definitions\
+        AttributeName=Artist,AttributeType=S \
+        AttributeName=AlbumTitle,AttributeType=S \
+    --global-secondary-index-updates \
+        "[{\"Create\":{\"IndexName\": \"ArtistAlbumTitle-index\",\"KeySchema\":[{\"AttributeName\":\"Artist\",\"KeyType\":\"HASH\"}, {\"AttributeName\":\"AlbumTitle\",\"KeyType\":\"RANGE\"}], \
+        \"ProvisionedThroughput\": {\"ReadCapacityUnits\": 10, \"WriteCapacityUnits\": 5      },\"Projection\":{\"ProjectionType\":\"ALL\"}}}]"
+
+#Criar um index global secundário baseado no título da música e no ano
+
+aws dynamodb update-table \
+    --table-name Music \
+    --attribute-definitions\
+        AttributeName=SongTitle,AttributeType=S \
+        AttributeName=SongYear,AttributeType=S \
+    --global-secondary-index-updates \
+        "[{\"Create\":{\"IndexName\": \"SongTitleYear-index\",\"KeySchema\":[{\"AttributeName\":\"SongTitle\",\"KeyType\":\"HASH\"}, {\"AttributeName\":\"SongYear\",\"KeyType\":\"RANGE\"}], \
+        \"ProvisionedThroughput\": {\"ReadCapacityUnits\": 10, \"WriteCapacityUnits\": 5      },\"Projection\":{\"ProjectionType\":\"ALL\"}}}]"
+
+#Querys para pesquisa no banco. Para executar, retire do comentario
+
+
+#Pesquisar item por artista
+#
+#aws dynamodb query \
+#    --table-name Music \
+#    --key-condition-expression "Artist = :artist" \
+#    --expression-attribute-values  '{":artist":{"S":"Iron Maiden"}}'
+
+
+#Pesquisar item por artista e título da música
+#
+#aws dynamodb query \
+#    --table-name Music \
+#    --key-condition-expression "Artist = :artist and SongTitle = :title" \
+#    --expression-attribute-values file://keyconditions.json
